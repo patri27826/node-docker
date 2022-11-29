53d2d48a0be531f45e26b2b9a55222b2e8ef568c
Patrick Lin <patrick41513@gmail.com>
2022-11-29 18:35:46 +0800

    mongo

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express")
+const mongoose = require('mongoose');
 
 const app = express()
+
+mongoose
+    .connect("mongodb://patrick:123@172.22.0.2:27017/?authSource=admin")
+    .then( () => console.log("successfully connected to mongo"))
+    .catch( (e) => console.error(e));
 
 app.get("/", (req,res) => {
     res.send("<h2>Welcome~~~~1213bbbbbb21321~</h2>")
